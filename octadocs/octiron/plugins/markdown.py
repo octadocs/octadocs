e1f912d649233a0e4d45d62fc5190fc01e9c8218
Anatoly Scherbakov <altaisoft@gmail.com>
2021-01-11 21:04:32 +0700

    #30 Make Markdown frontmatter YAML errors easier to debug

--- octadocs/octiron/plugins/markdown.py
+++ octadocs/octiron/plugins/markdown.py
@@ -1,12 +1,57 @@
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Iterator
 
 import frontmatter
 import rdflib
+from documented import DocumentedError
 from rdflib import RDF
+from yaml.scanner import ScannerError
 
 from octadocs.octiron.plugins import Loader
 from octadocs.octiron.types import OCTA, Triple
 from octadocs.octiron.yaml_extensions import as_triple_stream
+
+
+@dataclass
+class InvalidFrontmatter(DocumentedError):
+    """
+    Markdown Front Matter is not a valid YAML document.
+
+    Path: {self.path}
+
+    Read raw front matter below:
+
+    {self.raw_frontmatter}
+
+    Error:
+
+    {self.error}
+
+    {self.explanation}
+    """
+
+    path: Path
+    raw_frontmatter: str
+    error: ScannerError
+
+    @property
+    def explanation(self) -> str:
+        """Explain the error to the user."""
+        if 'found character \'\\t\' that' in str(self.error):
+            return 'Suggestion:\n  Use spaces, not tabs, for indentation.'
+
+        if self.error.problem == 'mapping values are not allowed here':
+            return (
+                'Suggestion:\n'
+                '  If property value contains ":", use quotes.\n\n'
+                '  Bad:\n\n'
+                '    label: FP for Sceptics: Intuitive guide to map/flatmap\n\n'
+                '  Good:\n\n'
+                '    label: "FP for Sceptics: Intuitive guide to map/flatmap"'
+            )
+
+        return ''
 
 
 class MarkdownLoader(Loader):
@@ -16,7 +61,29 @@ class MarkdownLoader(Loader):
 
     def stream(self) -> Iterator[Triple]:
         """Return stream of triples."""
-        meta_data = frontmatter.load(self.path).metadata
+        try:
+            meta_data = frontmatter.load(self.path).metadata
+        except ScannerError as err:
+            # Read the head of the file to print it in the error message
+            raw_frontmatter = []
+            with self.path.open() as raw_file:
+                for line in raw_file:
+                    if len(raw_frontmatter) > 10:
+                        raw_frontmatter.append('...')
+                        break
+
+                    if line.startswith('---') and raw_frontmatter:
+                        raw_frontmatter.append(line.strip('\n'))
+                        break
+
+                    raw_frontmatter.append(line.strip('\n'))
+
+            raise InvalidFrontmatter(
+                path=self.path,
+                raw_frontmatter='\n'.join(raw_frontmatter),
+                error=err,
+            ) from err
+
         yield from as_triple_stream(
             raw_data=meta_data,
             context=self.context,
