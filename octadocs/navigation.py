aaeeda0b6cf4ce293f85e32b74ca203b893bf6e0
Anatoly Scherbakov <altaisoft@gmail.com>
2021-03-12 00:13:22 +0700

    One more intermediate commit

--- octadocs/navigation.py
+++ octadocs/navigation.py
@@ -1,10 +1,13 @@
 import functools
 import sys
 from dataclasses import dataclass
-from typing import Union, NamedTuple
+from typing import Union, NamedTuple, Dict, List
 
 import rdflib
-from mkdocs.structure.nav import Link, Navigation, Section
+from mkdocs.structure.nav import (
+    Link, Navigation, Section,
+    _add_previous_and_next_links,
+)
 from mkdocs.structure.pages import Page
 
 from octadocs.conversions import iri_by_page
@@ -17,6 +20,10 @@ else:
 
 
 NavigationItem = Union[Page, Section, Link]
+
+DEFAULT_SECTION_POSITION = -200
+INDEX_MD_DEFAULT_POSITION = -100
+PAGE_DEFAULT_POSITION = 0
 
 
 def is_index_md(navigation_item: NavigationItem) -> bool:
@@ -106,9 +113,46 @@ class OctadocsNavigationProcessor:
     graph: rdflib.ConjunctiveGraph
     navigation: Navigation
 
+    @cached_property
+    def position_by_page(self) -> Dict[rdflib.URIRef, int]:
+        """Fetch the position by page from graph."""
+        query_text = '''
+            SELECT ?page ?position WHERE {
+                ?page
+                    a octa:Page ;
+                    octa:position ?position .
+            }
+        '''
+
+        rows = query(
+            query_text=query_text,
+            instance=self.graph,
+        )
+
+        return {
+            row['page']: row['position'].value
+            for row in rows
+        }
+
+    def assign_positions_to_pages(self) -> None:
+        """Go through every page and assign position there."""
+        page: Page
+        for page in self.navigation.pages:
+            iri = iri_by_page(page)
+
+            default_position = INDEX_MD_DEFAULT_POSITION if (
+                is_index_md(page)
+            ) else PAGE_DEFAULT_POSITION
+
+            page.position = self.position_by_page.get(
+                iri,
+                default_position,
+            )
+
     def generate(self) -> Navigation:
         """Generate the navigation structure."""
-        return assign_position_and_reorder(self.navigation)
+        self.assign_positions_to_pages()
+        return reorder_navigation(self.navigation)
 
     def _process_nav_item(self, item: NavigationItem) -> NavigationItem:
         if isinstance(item, Page):
@@ -129,3 +173,63 @@ class OctadocsNavigationProcessor:
     def _process_nav_section(self, section: Section) -> NavigationItem:
         """Process section."""
         # return assign_position_and_reorder()
+
+
+@functools.singledispatch
+def reorder_navigation(navigation_item):
+    """Reorder navigation items based on their positions."""
+    raise NotImplementedError(f'Cannot reorder {navigation_item}.')
+
+
+@reorder_navigation.register(Page)
+def _reorder_page(page: Page) -> Page:
+    """Nothing to reorder here, page is a page."""
+    return page
+
+
+def navigation_sort_key(navigation_item: NavigationItem) -> SortKey:
+    return SortKey(
+        getattr(navigation_item, 'position', DEFAULT_SECTION_POSITION),
+        navigation_item.title,
+    )
+
+
+@reorder_navigation.register(Section)
+def _reorder_section(section: Section) -> Section:
+    """Reorder items inside a section based on their priorities."""
+    section.children = list(map(
+        reorder_navigation,
+        section.children,
+    ))
+
+    section.children.sort(
+        key=navigation_sort_key,
+    )
+
+    return section
+
+
+def construct_navigation_pages_by_items(
+    navigation_items: List[NavigationItem],
+) -> List[Page]:
+    """Construct flat pages list iterating by navigation three."""
+    pages = []
+    raise Exception('AAA!!!')
+
+
+@reorder_navigation.register(Navigation)
+def _reorder_navigation(navigation: Navigation) -> Navigation:
+    """Reorder items in the root navigation."""
+    navigation.items = list(map(
+        reorder_navigation,
+        navigation.items,
+    ))
+
+    navigation.items.sort(
+        key=navigation_sort_key,
+    )
+
+    navigation_pages = construct_navigation_pages_by_items()
+    _add_previous_and_next_links(navigation.pages)
+
+    return navigation
